2ab6be3f2a9736602420d45d3b1a5ee67f915c65
KarolineH <karoline@heiwolt.de>
2023-11-22 15:50:38 +0000

    changed repo structure

--- eos_interface/my_node.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print('Hi from eos_interface.')
-
-
-if __name__ == '__main__':
-    main()
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-package_name = 'eos_interface'
+package_name = 'ros2_cam_if'
 
 setup(
     name=package_name,
@@ -15,12 +15,12 @@ setup(
     zip_safe=True,
     maintainer='karoline',
     maintainer_email='karoline@heiwolt.de',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='This package is ROS wrapper, containing a remote control and I/O interface for a Canon EOS R5 C camera',
+    license='MIT License, Copyright 2023 Karoline Heiwolt',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'my_node = eos_interface.my_node:main'
+            'my_node = ros2_cam_if.my_node:main'
         ],
     },
 )
