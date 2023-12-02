798e7680481cbd3a30842183ae5182b6d65a01ea
KarolineH <karoline@heiwolt.de>
2023-12-02 15:41:49 +0000

    updated message types, and first ready-to-build version

--- ros2_cam_if/eos_node.py
+++ ros2_cam_if/eos_node.py
@@ -5,23 +5,23 @@ from rclpy.action import ActionServer
 import time
 import os 
 
-# from .cam_interface.capture import EOS
-# from .cam_interface import gphoto_util
-from cam_interface.capture import EOS
-from cam_interface import gphoto_util
+from .cam_interface.capture import EOS
+from .cam_interface import gphoto_util
+# from cam_interface.capture import EOS
+# from cam_interface import gphoto_util
 
-from cam_types.action import Capture
-from cam_types.srv import Config
+from capture_types.action import Capture
+from capture_types.srv import CameraConfig
 
 class EOS_node(Node):
 
     def __init__(self):
         super().__init__('eos_cam')
 
-        # self.declare_parameter('port', rclpy.Parameter.Type.STRING) # USB port address, to be set in the launch file
-        # self.declare_parameter('target_path', rclpy.Parameter.Type.STRING) # path to the target folder for image and vid downloads, to be set in the launch file
-        self.declare_parameter('port', 'usb:002,020')
-        self.declare_parameter('target_path', '/home/karoline/rosws/media')
+        self.declare_parameter('port', rclpy.Parameter.Type.STRING) # USB port address, to be set in the launch file
+        self.declare_parameter('target_path', rclpy.Parameter.Type.STRING) # path to the target folder for image and vid downloads, to be set in the launch file
+        # self.declare_parameter('port', 'usb:002,020')
+        # self.declare_parameter('target_path', '/home/karoline/rosws/media')
         # when initializing the camera, please make sure to specify a unique target path for media downloads, otherwise files might be overwritten
 
         try:
@@ -38,19 +38,19 @@ class EOS_node(Node):
         self.cam = EOS(port=port.value)
         
         # set up services
-        self.sync_date_time_srv = self.create_service(Config, '~/sync_date_time', self.sync_date_time)
-        self.drive_focus_srv = self.create_service(Config, '~/drive_focus', self.drive_focus)
-        self.get_ap_srv = self.create_service(Config, '~/get_aperture', self.get_aperture)
-        self.set_ap_srv = self.create_service(Config, '~/set_aperture', self.set_aperture)
-        self.get_ss_srv = self.create_service(Config, '~/get_shutterspeed', self.get_shutter_speed)
-        self.set_ss_srv = self.create_service(Config, '~/set_shutterspeed', self.set_shutter_speed)
-        self.get_iso_srv = self.create_service(Config, '~/get_iso', self.get_iso)
-        self.set_iso_srv = self.create_service(Config, '~/set_iso', self.set_iso)
-        self.set_contaf_srv = self.create_service(Config, '~/set_continuous_af', self.set_cont_af)
-        self.get_imform_srv = self.create_service(Config, '~/get_image_format', self.get_imform)
-        self.set_imform_srv = self.create_service(Config, '~/set_image_format', self.set_imform)
-        self.set_afpoint_srv = self.create_service(Config, '~/set_af_point', self.set_afpoint)
-        self.trigger_af_srv = self.create_service(Config, '~/trigger_af', self.trigger_AF)
+        self.sync_date_time_srv = self.create_service(CameraConfig, '~/sync_date_time', self.sync_date_time)
+        self.drive_focus_srv = self.create_service(CameraConfig, '~/drive_focus', self.drive_focus)
+        self.get_ap_srv = self.create_service(CameraConfig, '~/get_aperture', self.get_aperture)
+        self.set_ap_srv = self.create_service(CameraConfig, '~/set_aperture', self.set_aperture)
+        self.get_ss_srv = self.create_service(CameraConfig, '~/get_shutterspeed', self.get_shutter_speed)
+        self.set_ss_srv = self.create_service(CameraConfig, '~/set_shutterspeed', self.set_shutter_speed)
+        self.get_iso_srv = self.create_service(CameraConfig, '~/get_iso', self.get_iso)
+        self.set_iso_srv = self.create_service(CameraConfig, '~/set_iso', self.set_iso)
+        self.set_contaf_srv = self.create_service(CameraConfig, '~/set_continuous_af', self.set_cont_af)
+        self.get_imform_srv = self.create_service(CameraConfig, '~/get_image_format', self.get_imform)
+        self.set_imform_srv = self.create_service(CameraConfig, '~/set_image_format', self.set_imform)
+        self.set_afpoint_srv = self.create_service(CameraConfig, '~/set_af_point', self.set_afpoint)
+        self.trigger_af_srv = self.create_service(CameraConfig, '~/trigger_af', self.trigger_AF)
 
         # set up actions
         self._single_capture_svr = ActionServer(self,Capture,'~/single_capture',self.single_capture)
@@ -70,13 +70,13 @@ class EOS_node(Node):
         # 0,1,2 == small, medium, large increment --> nearer
         # 3 == none
         # 4,5,6 == small, medium, large increment --> further 
-        if len(request.val1) > 0:
-            msg = self.cam.manual_focus(value=int(request.val1))
+        if len(request.x) > 0:
+            msg = self.cam.manual_focus(value=int(request.x))
             response.output_msg = msg
             return response
         else:
             warnings.warn("No value passed to the manual focus drive service. Focus has not been adjusted.")
-            response.output_msg = 'error, missing val1'
+            response.output_msg = 'error, missing value x'
             return response
     
     def get_aperture(self,request,response):
@@ -87,7 +87,7 @@ class EOS_node(Node):
         return response
     
     def set_aperture(self,request,response):
-        current, choices, msg = self.cam.set_aperture(value=request.val1, list_choices=False)
+        current, choices, msg = self.cam.set_aperture(value=request.x, list_choices=False)
         response.current_val = current
         response.choices = [str(choice) for choice in choices]
         response.output_msg = msg
@@ -101,7 +101,7 @@ class EOS_node(Node):
         return response
     
     def set_shutter_speed(self,request,response):
-        current, choices, msg = self.cam.set_shutterspeed(value=request.val1, list_choices=False)
+        current, choices, msg = self.cam.set_shutterspeed(value=request.x, list_choices=False)
         response.current_val = current
         response.choices = [str(choice) for choice in choices]
         response.output_msg = msg
@@ -115,14 +115,14 @@ class EOS_node(Node):
         return response
     
     def set_iso(self,request,response):
-        current, choices, msg = self.cam.set_iso(value=request.val1, list_choices=False)
+        current, choices, msg = self.cam.set_iso(value=request.x, list_choices=False)
         response.current_val = current
         response.choices = [str(choice) for choice in choices]
         response.output_msg = msg
         return response
     
     def set_cont_af(self,request,response):
-        current, msg = self.cam.set_continuous_AF(value=request.val1)
+        current, msg = self.cam.set_continuous_AF(value=request.x)
         response.current_val = current
         response.output_msg = msg
         return response
@@ -135,7 +135,7 @@ class EOS_node(Node):
         return response
     
     def set_imform(self,request,response):
-        current, choices, msg = self.cam.set_image_format(value=request.val1, list_choices=False)
+        current, choices, msg = self.cam.set_image_format(value=request.x, list_choices=False)
         response.current_val = current
         response.choices = choices
         response.output_msg = msg
@@ -143,8 +143,8 @@ class EOS_node(Node):
     
     def set_afpoint(self,request,response):
         try:
-            x = int(request.val1)
-            y = int(request.val2)
+            x = int(request.x)
+            y = int(request.y)
         except:
             response.output_msg = 'error, x and y coordinates must be integers, check for any trailing spaces or zeros'
             return response
